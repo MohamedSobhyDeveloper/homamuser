e6ad26d02983da79460ba538ec0116c6775f0ed6
Mohamed <ms2949550@gmail.com>
2021-02-11 12:11:41 +0200

    first commit development

--- app/src/main/java/com/otex/homamuser/view/login/LoginActivity.kt
+++ app/src/main/java/com/otex/homamuser/view/login/LoginActivity.kt
@@ -2,11 +2,26 @@ package com.otex.homamuser.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.otex.homamuser.R
+import com.otex.homamuser.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private var loginviewmodel : LoginActivityViewModel? = null
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initialize()
+
+    }
+
+    private fun initialize() {
+        loginviewmodel = ViewModelProvider(this).get(LoginActivityViewModel::class.java)
+
     }
 }
