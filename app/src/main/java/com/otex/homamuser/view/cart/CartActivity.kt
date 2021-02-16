89c049c13443a378fa9e2023d6281990beb2af74
Mamdouh <mamdouhsobhy923@gmail.com>
2021-02-16 12:02:50 -0800

    mamdouh_restaurantlist

---
+++ app/src/main/java/com/otex/homamuser/view/cart/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.otex.homamuser.view.cart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.otex.homamuser.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
