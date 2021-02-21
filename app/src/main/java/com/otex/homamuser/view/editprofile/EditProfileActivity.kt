0b6eee65ff1e6a5339ff31e05a9540794dbc911a
Mamdouh <mamdouhsobhy923@gmail.com>
2021-02-21 06:54:01 -0800

    mamdouh_validate

--- app/src/main/java/com/otex/homamuser/view/editprofile/EditProfileActivity.kt
+++ app/src/main/java/com/otex/homamuser/view/editprofile/EditProfileActivity.kt
@@ -3,6 +3,7 @@ package com.otex.homamuser.view.editprofile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.otex.homamuser.R
 import com.otex.homamuser.databinding.ActivityEditProfileBinding
@@ -38,8 +39,41 @@ class EditProfileActivity : AppCompatActivity() {
             startActivity(Intent(this, MyProfileActivity::class.java))
             finish()
         }
+        binding.btnSave.setOnClickListener {
+            username=binding.editUsername.text.toString()
+            email_or_phone=binding.editEmailPhone.text.toString()
+            phone=binding.editPhone.text.toString()
+            password=binding.editPassword.text.toString()
+            confirm_pass=binding.editPasswordConfirm.text.toString()
 
+            if(username.equals("")){
+                binding.editUsername.setError(getString(R.string.enter_username))
+            }else if(email_or_phone.equals("")){
+                binding.editEmailPhone.setError(getString(R.string.enter_email_phone))
+            }else if(phone.equals("")){
+                binding.editPhone.setError(getString(R.string.enter_phone))
 
+            }else if( password.equals("")){
+                binding.editPassword.setError(getString(R.string.enter_password))
+
+            }else if(confirm_pass.equals("")){
+                binding.editPasswordConfirm.setError(getString(R.string.enter_retype_pass))
+
+            }else{
+                if(confirm_pass==password){
+                    update_profile()
+                }else{
+                    binding.editPasswordConfirm.setError(getString(R.string.dosent_match))
+                }
+            }
+
+        }
+
+    }
+
+    private fun update_profile() {
+
+        Toast.makeText(this,"Done!",Toast.LENGTH_LONG).show()
     }
 
     private fun initialize() {
