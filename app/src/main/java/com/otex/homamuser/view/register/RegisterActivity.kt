6573cdfe4f021a95fb30b5f33d2b00ba12ffff26
Sobhy <mohamed.sobhy@kickstartinteractive.net>
2021-06-17 14:09:36 +0200

    [mohamed][14/3]some changes

--- app/src/main/java/com/otex/homamuser/view/register/RegisterActivity.kt
+++ app/src/main/java/com/otex/homamuser/view/register/RegisterActivity.kt
@@ -12,28 +12,29 @@ import com.otex.homamuser.view.login.LoginActivity
 import java.util.*
 
 class RegisterActivity : BaseActivity() {
-    private var registerActivityViewModel : RegisterActivityViewModel? = null
+    private var registerActivityViewModel: RegisterActivityViewModel? = null
     lateinit var binding: ActivityRegisterBinding
-    private  var emailOrPhone:String=""
-    private var password:String=""
-    private var confirmPassword:String=""
-    private  var phone:String=""
-    private var username:String=""
-    private var userInfo:UserInfo?=null
+    private var emailOrPhone: String = ""
+    private var password: String = ""
+    private var confirmPassword: String = ""
+    private var phone: String = ""
+    private var username: String = ""
+    private var userInfo: UserInfo? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initialize()
-         click()
+        click()
     }
 
     private fun initialize() {
-        registerActivityViewModel = ViewModelProvider(this).get(RegisterActivityViewModel::class.java)
+        registerActivityViewModel =
+            ViewModelProvider(this).get(RegisterActivityViewModel::class.java)
         registerActivityViewModel!!.registerLivedata.observe(this) {
-                val intent =Intent(this,VerificationCodeActivity::class.java)
-                intent.putExtra("phone",it.user.phone)
-                startActivity(intent)
+            val intent = Intent(this, VerificationCodeActivity::class.java)
+            intent.putExtra("phone", it.user.phone)
+            startActivity(intent)
 //               startActivity(Intent(this, LoginActivity::class.java))
 //             finish()
 //             Toast.makeText(this,it.message+"",Toast.LENGTH_SHORT).show()
@@ -60,35 +61,34 @@ class RegisterActivity : BaseActivity() {
 
     private fun makeRegister() {
         assimentVariable()
-        if(username == ""){
+        if (username == "") {
             binding.editUsername.error = getString(R.string.enter_username)
-        }else if(phone == ""){
+        } else if (phone == "") {
             binding.editPhone.error = getString(R.string.enter_phone)
-        }else if(password == ""){
+        } else if (password == "") {
             binding.editPassword.error = getString(R.string.enter_password)
-        }else if(confirmPassword == ""){
+        } else if (confirmPassword == "") {
             binding.editPasswordConfirm.error = getString(R.string.enter_password)
-        }else{
-            if(confirmPassword==password){
-                register(username,emailOrPhone,password,phone)
-            }else{
+        } else {
+            if (confirmPassword == password) {
+                register(username, emailOrPhone, password, phone)
+            } else {
                 binding.editPasswordConfirm.error = getString(R.string.confirm_not_equal_password)
             }
         }
     }
 
     private fun assimentVariable() {
-        username=binding.editUsername.text.toString()
-        emailOrPhone=binding.editEmailPhone.text.toString()
-        phone=binding.editPhone.text.toString()
-        password=binding.editPassword.text.toString()
-        confirmPassword=binding.editPasswordConfirm.text.toString()
+        username = binding.editUsername.text.toString()
+        emailOrPhone = binding.editEmailPhone.text.toString()
+        phone = binding.editPhone.text.toString()
+        password = binding.editPassword.text.toString()
+        confirmPassword = binding.editPasswordConfirm.text.toString()
     }
 
     private fun register(username: String, emailOrPhone: String, password: String, phone: String) {
 
         val map = HashMap<String, String?>()
-        map["email"] = phone
         map["password"] = password
         map["name"] = username
         map["phone"] = phone
